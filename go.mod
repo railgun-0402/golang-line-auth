6733a9c5a4c597f9685097149b8e65a06390d30b
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-01-22 23:07:34 +0900

    一旦ハンドラー作成

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/railgun-0402/golang-line-auth
 
 go 1.23.1
+
+require golang.org/x/oauth2 v0.25.0 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,31 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
+
+// LINE認証に必要な値を管理
+// TODO: 環境変数化
+const (
+	clientID = ""
+	redirectURI = ""
+	leinAuthURL = ""
+)
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// 認証URLの構築
+	authURL, _ := url.Parse(leinAuthURL)
+	query := authURL.Query()
+	query.Set("response_type", "code")
+	query.Set("client_id", clientID)
+	query.Set("redirect_uri", redirectURI)
+	query.Set("state", "random_state_string")
+	query.Set("scope", "profile openid email")
+	authURL.RawQuery = query.Encode()
+
+	// redirect
+	http.Redirect(w, r, authURL.String(), http.StatusFound)
+}
 
 func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
